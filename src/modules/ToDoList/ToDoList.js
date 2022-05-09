a66a0f5fd18d068af49e7c89d2062e07564b5697
Facundo Tolosa <facundotolosa98@gmail.com>
2022-05-09 18:14:14 +0200

    Read with redux created

--- src/modules/ToDoList/ToDoList.js
+++ src/modules/ToDoList/ToDoList.js
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadListActionCreator } from "../../redux/features/listSlice";
 import toDoListData from "../../utils/toDoListData";
 import ToDoListItem from "../ToDoListItem/ToDoListItem";
 
 const ToDoList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadListActionCreator(toDoListData));
+  }, [dispatch]);
+
+  const list = useSelector((state) => state.list);
+
   return (
     <ul>
-      {toDoListData.map((toDoListItem) => {
-        return (
-          <ToDoListItem toDoListItem={toDoListItem} key={toDoListItem.id} />
-        );
+      {list.map((listItem) => {
+        return <ToDoListItem toDoListItem={listItem} key={listItem.id} />;
       })}
     </ul>
   );
